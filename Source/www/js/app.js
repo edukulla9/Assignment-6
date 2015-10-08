10d3491c7158f571aee0e08f51409b81239cd54c
edukulla9 <edukulla.sadhana9@gmail.com>
2015-10-08 15:39:43 -0600

    Assignment 6
    Lab 6

---
+++ Source/www/js/app.js
@@ -0,0 +1,32 @@
+angular.module('starter', ['ionic', 'starter.services'])
+
+.config(function($compileProvider){
+  $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel):/);
+})
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.controller('MainCtrl', function($scope, Camera) {
+
+  $scope.getPhoto = function() {
+    Camera.getPicture().then(function(imageURI) {
+      console.log(imageURI);
+      $scope.lastPhoto = imageURI;
+    }, function(err) {
+      console.err(err);
+    }, {
+      quality: 75,
+      targetWidth: 320,
+      targetHeight: 320,
+      saveToPhotoAlbum: false
+    });
+  };
+
+})
